9a3596ac0c3b96e226ed3584eaf3a73e10c05588
Usama Tariq <37779044+UsaMaH11@users.noreply.github.com>
2026-03-25 19:51:55 +0500

    Update footer details in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,16 @@
-// import components
-import Hero from "./components/Hero";
-import Navbar from "./Layouts/Navbar";
-import Skills from "./components/Skills";
-import Service from "./components/Services";
-import Projects from "./components/Projects";
-import Testimonials from "./components/Testimonials";
-import Hireme from "./components/Hireme";
-import Contact from "./components/Contact";
-import { useEffect } from "react";
-// Animation package
-import Aos from "aos";
-import "aos/dist/aos.css";
+import React from 'react';
+import './App.css';
 
-const App = () => {
-  useEffect(() => {
-    Aos.init({
-      duration: 1800,
-      offset: 100,
-      disable: "mobile",
-    });
-  }, []);
-  return (
-    <div className="">
-      <Navbar />
-      <Hero />
-      <Skills />
-      <Service />
-      <Projects />
-      <Testimonials />
-      <Hireme />
-      <Contact />
-      <footer className="p-3 text-center">
-        <h6 className="mb-3">JOHN ALEX</h6>
-        <p>codeaprogram © All CopyRights Reserved 2022</p>
-      </footer>
-    </div>
-  );
-};
+function App() {
+    return (
+        <div className="App">
+            {/* Other components */}  
+            <footer>
+                <h6>USAMA TARIQ</h6>
+                <p>Senior Backend Engineer @ Manafa | © 2026 All Rights Reserved</p>
+            </footer>
+        </div>
+    );
+}
 
 export default App;
